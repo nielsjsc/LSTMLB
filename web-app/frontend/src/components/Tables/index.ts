16f95243d2ba4e757898e88bc9bfc4cdafb67ff6
Niels Christoffersen <70546911+nielsjsc@users.noreply.github.com>
2025-02-21 18:56:08 -0800

    Update index.ts
    removed default stat table

--- web-app/frontend/src/components/Tables/index.ts
+++ web-app/frontend/src/components/Tables/index.ts
@@ -1,3 +1,2 @@
-export { default as StatTable } from './BaseTable/StatTable';
 export { default as CombinedHittingTable } from './PlayerTables/CombinedHittingTable';
 export { default as CombinedPitchingTable } from './PlayerTables/CombinedPitchingTable';
